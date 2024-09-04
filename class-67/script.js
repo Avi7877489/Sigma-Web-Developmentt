0909eced2a2471ac09839e81550ce45777063c6d
Avi7877489 <150539120+Avi7877489@users.noreply.github.com>
2024-09-04 20:20:41 +0530

    Update Script.js

--- class-67/script.js
+++ class-67/script.js
@@ -1,16 +1,16 @@
 console.log("Hello World")
 
-document.body.firstElementChild
-document.body.firstElementChild.childNodes
-document.body.firstElementChild.children
-document.body.firstElementChild.children[3].nextElementSibling
-document.body.firstElementChild.children[3].previousElementSibling
+// document.body.firstElementChild
+// document.body.firstElementChild.childNodes
+// document.body.firstElementChild.children
+// document.body.firstElementChild.children[3].nextElementSibling
+// document.body.firstElementChild.children[3].previousElementSibling
 
-document.body.children
+// document.body.children
 
-document.body.children[1]
+// document.body.children[1]
 
-document.body.children[1].row
-document.body.children[1].rows
+// document.body.children[1].row
+// document.body.children[1].rows
 
-document.body.children[1].caption
+// document.body.children[1].caption
